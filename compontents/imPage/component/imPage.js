284ad203364d798ebb1cb96258839a955f3639a7
王广峰 <wanggf@xiaoshouyi.com>
2017-12-07 18:19:58 +0800

    update

--- compontents/imPage/component/imPage.js
+++ compontents/imPage/component/imPage.js
@@ -4,15 +4,28 @@ import {
     LayoutAnimation,
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Platform,
+    TouchableOpacity,
+    Dimensions
 } from 'react-native';
 
 import Immutable,{Map,List,Set} from 'immutable';
 import Cursor from '../../../node_modules/immutable/contrib/cursor';
 
+let screen = Dimensions.get('window');
+
 class imPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            w:200,
+            h:100
+        }
+    }
 
     componentWillMount() {
+
         // alert('impage.........'+JSON.stringify(this.props.full));
 
         // let one = Immutable.fromJS({a:{b:1}});
@@ -34,22 +47,49 @@ class imPage extends Component {
         // });
         // cursor = cursor.update('c', x => x + 1);
         // cursor.get('c');
+
+        // alert(Platform.OS);
     }
 
     render() {
         return (
             <Animated.View style={styles.container}>
-                <Text key='000' style={{lineHeight:50}}>IM首页</Text>
-                {Object.keys(this.props.state).map(key => <Text key={key+''}>{this.props.state[key+''].name}</Text>)}
+                <View style={{width:screen.width,height:100,backgroundColor:'pink'}}>
+                    <Text key='000' style={{lineHeight:50}}>IM首页</Text>
+                    {Object.keys(this.props.state).map(key => <Text key={key+''}>{this.props.state[key+''].name}</Text>)}
+                </View>
+
+                <View style={{width: this.state.w, height: this.state.h, backgroundColor:'red'}}>
+                    <TouchableOpacity style={{width:100,height:40,backgroundColor:'green',margin:5}} onPress={() => this._onPress()}>
+                        <Text>点击</Text>
+                    </TouchableOpacity>
+                </View>
+
             </Animated.View>
         );
     }
+
+    _onPress() {
+        LayoutAnimation.configureNext({
+            duration: 700,   //持续时间
+            create: {
+                type: 'linear',
+                property: 'opacity'
+            },
+            update: {
+                type: 'spring',
+                springDamping: 0.4
+            }
+        });
+        this.setState({w: this.state.w - 20, h: this.state.h - 20})
+    }
+
 }
 
 let styles = StyleSheet.create({
     container:{
         flex:1,
-        backgroundColor:'#00ff00'
+        // backgroundColor:'#00ff00'
     }
 });
 
