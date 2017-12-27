9bb59f49b8492121a3d020bfff8fd62aacb5d420
王广峰 <wanggf@xiaoshouyi.com>
2017-12-27 12:09:06 +0800

    加tabBar

--- compontents/imPage/component/imPage.js
+++ compontents/imPage/component/imPage.js
@@ -1,21 +1,21 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import {
-    Animated,
-    LayoutAnimation,
-    View,
-    Text,
-    StyleSheet,
-    Platform,
-    TouchableOpacity,
-    Dimensions,
-    Easing,
-    PanResponder,
-    ScrollView,
-    Image
+  Animated,
+  LayoutAnimation,
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  TouchableOpacity,
+  Dimensions,
+  Easing,
+  PanResponder,
+  ScrollView,
+  Image
 } from 'react-native';
 import { Actions, ActionConst } from 'react-native-router-flux';
 
-import Immutable,{Map,List,Set} from 'immutable';
+import Immutable, { Map, List, Set } from 'immutable';
 import Cursor from '../../../node_modules/immutable/contrib/cursor';
 import TwoPage from './twoPage';
 import SvgPage from './svgPage';
@@ -24,79 +24,93 @@ import TestPage from './testPage';
 let screen = Dimensions.get('window');
 
 class imPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            w:200,
-            h:100,
-            ov:new Animated.Value(1),
-            bounceValue:new Animated.Value(1),
-            transY:new Animated.Value(100),
-            postion:new Animated.ValueXY({x:20,y:100}),
-            eventValue:new Animated.Value(1.0)
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      w: 200,
+      h: 100,
+      ov: new Animated.Value(1),
+      bounceValue: new Animated.Value(1),
+      transY: new Animated.Value(100),
+      postion: new Animated.ValueXY({ x: 20, y: 100 }),
+      eventValue: new Animated.Value(1.0)
     }
+  }
 
-    componentWillMount() {
+  componentWillMount() {
 
-        // alert('impage.........'+JSON.stringify(this.props.full));
+    // alert('impage.........'+JSON.stringify(this.props.full));
 
-        // let one = Immutable.fromJS({a:{b:1}});
-        // let two = one.ç(['a','b'],2);
-        // console.log('one........',one.getIn(['a','b']));
-        // console.log('two........',two.getIn(['a','b']));
+    // let one = Immutable.fromJS({a:{b:1}});
+    // let two = one.ç(['a','b'],2);
+    // console.log('one........',one.getIn(['a','b']));
+    // console.log('two........',two.getIn(['a','b']));
 
-        // let a = Map({
-        //     name:'xiaofeng',
-        //     address:{city:'beijing',zone:'chaoyang'}
-        // });
-        // let b = a.set('name','shanghai');
-        // console.log('a.....',a.toJS());
-        // console.log('b.....',b.get('address'));
+    // let a = Map({
+    //     name:'xiaofeng',
+    //     address:{city:'beijing',zone:'chaoyang'}
+    // });
+    // let b = a.set('name','shanghai');
+    // console.log('a.....',a.toJS());
+    // console.log('b.....',b.get('address'));
 
-        // let a = Immutable.fromJS({a:{b:{c:1}}});
-        // let cursor = Cursor.from(a,['a','b'], newData => {
-        //     console.log('aaa.....',newData.toJS());
-        // });
-        // cursor = cursor.update('c', x => x + 1);
-        // cursor.get('c');
+    // let a = Immutable.fromJS({a:{b:{c:1}}});
+    // let cursor = Cursor.from(a,['a','b'], newData => {
+    //     console.log('aaa.....',newData.toJS());
+    // });
+    // cursor = cursor.update('c', x => x + 1);
+    // cursor.get('c');
 
-        // alert(Platform.OS);
-    }
+    // alert(Platform.OS);
+  }
 
-    render() {
-        return (
-            <Animated.View style={styles.container}>
-                <View style={{width:screen.width,height:100,backgroundColor:'pink'}}>
-                    <Text key='000' style={{lineHeight:50}}>IM首页</Text>
-                    {Object.keys(this.props.state).map(key => <Text key={key+''}>{this.props.state[key+''].name}</Text>)}
-                </View>
+  render() {
+    return (
+      <Animated.View style={styles.container}>
+        <View style={{ width: screen.width, height: 100, backgroundColor: 'pink' }}>
+          <Text key='000' style={{ lineHeight: 50 }}>IM首页</Text>
+          {Object.keys(this.props.state).map(key => <Text key={key + ''}>{this.props.state[key + ''].name}</Text>)}
+        </View>
 
-                <Animated.View style={{flex:1,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
-                    <TouchableOpacity 
-                        style={{margin:2,width:100,height:40,backgroundColor:'brown',justifyContent:'center',alignItems:'center'}}
-                        onPress={ () => Actions.tab_main()}>
-                            <Text>切换Tab</Text>
-                    </TouchableOpacity>
+        <Animated.View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'space-around'
+          }}>
 
-                    <TouchableOpacity 
-                        style={{margin:2,width:100,height:40,backgroundColor:'brown',justifyContent:'center',alignItems:'center'}}
-                        onPress={ () => Actions.home_two()}>
-                            <Text>同Tab内push</Text>
-                    </TouchableOpacity>
-                
-                    <TouchableOpacity 
-                        style={{margin:2,width:100,height:40,backgroundColor:'brown',justifyContent:'center',alignItems:'center'}}
-                        onPress={ () => Actions.login()}>
-                            <Text>Modal</Text>
-                    </TouchableOpacity>
-                </Animated.View>
+          <TouchableOpacity
+            style={{ margin: 2, width: 85, height: 40, backgroundColor: 'brown', justifyContent: 'center', alignItems: 'center' }}
+            onPress={() => Actions.tab_main()}>
+            <Text>切换Tab</Text>
+          </TouchableOpacity>
 
-                <TwoPage />
-                <SvgPage active={false} />
-                <TestPage {...this.props} />
+          <TouchableOpacity
+            style={{ margin: 2, width: 85, height: 40, backgroundColor: 'brown', justifyContent: 'center', alignItems: 'center' }}
+            onPress={() => Actions.home_two()}>
+            <Text>同Tab内push</Text>
+          </TouchableOpacity>
 
-                {/* <Animated.View ref='dongView' 
+          <TouchableOpacity
+            style={{ margin: 2, width: 85, height: 40, backgroundColor: 'brown', justifyContent: 'center', alignItems: 'center' }}
+            onPress={() => Actions.login()}>
+            <Text>Modal</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{ margin: 2, width: 85, height: 40, backgroundColor: 'brown', justifyContent: 'center', alignItems: 'center' }}
+            onPress={() => Actions.lightbox_one()}>
+            <Text>Drawer</Text>
+          </TouchableOpacity>
+
+        </Animated.View>
+
+        <TwoPage />
+        <SvgPage active={false} />
+        <TestPage {...this.props} />
+
+        {/* <Animated.View ref='dongView' 
                 style={{
                     width: this.state.w, 
                     height: this.state.h, 
@@ -112,7 +126,7 @@ class imPage extends Component {
                     </TouchableOpacity>
                 </Animated.View> */}
 
-                {/* <ScrollView 
+        {/* <ScrollView 
                     horizontal={false}
                     showsHorizontalScrollIndicator={false}
                     showsVerticalScrollIndicator={true}
@@ -130,68 +144,68 @@ class imPage extends Component {
                         }} />
                 </ScrollView> */}
 
-                
 
-            </Animated.View>
-        );
-    }
 
-    _onPress() {
-        // LayoutAnimation.configureNext({
-        //     duration: 700,   //持续时间
-        //     create: {
-        //         type: 'linear',
-        //         property: 'opacity'
-        //     },
-        //     update: {
-        //         type: 'spring',
-        //         springDamping: 0.4
-        //     }
-        // });
-        // this.setState({w: this.state.w - 20, h: this.state.h - 20});
+      </Animated.View>
+    );
+  }
 
-        // Animated.timing(this.state.ov, {
-        //     toValue:0,
-        //     duration:3000,
-        //     delay:3000,
-        //     easing:Easing.bezier(0.15, 0.73, 0.37, 1.2),
-        //     useNativeDriver:true
-        // }).start();
+  _onPress() {
+    // LayoutAnimation.configureNext({
+    //     duration: 700,   //持续时间
+    //     create: {
+    //         type: 'linear',
+    //         property: 'opacity'
+    //     },
+    //     update: {
+    //         type: 'spring',
+    //         springDamping: 0.4
+    //     }
+    // });
+    // this.setState({w: this.state.w - 20, h: this.state.h - 20});
 
-        // this.state.postion.setValue({x:20,y:100});
-        Animated.spring(
-            this.state.postion,{
-                toValue:{x:50,y:200},
-                friction: 7,     //摩擦系数
-                tension: 40,      //张力系数
-                // bounciness:5,
-                // speed:5
-              }
-        ).start();
+    // Animated.timing(this.state.ov, {
+    //     toValue:0,
+    //     duration:3000,
+    //     delay:3000,
+    //     easing:Easing.bezier(0.15, 0.73, 0.37, 1.2),
+    //     useNativeDriver:true
+    // }).start();
 
-        // Animated.decay(
-        //     this.state.bounceValue,{
-        //         toValue:0.2,
-        //         velocity: 0.1,      //速度
-        //         deceleration: 0.9   //哀减
-        //       }
-        // ).start();
+    // this.state.postion.setValue({x:20,y:100});
+    Animated.spring(
+      this.state.postion, {
+        toValue: { x: 50, y: 200 },
+        friction: 7,     //摩擦系数
+        tension: 40,      //张力系数
+        // bounciness:5,
+        // speed:5
+      }
+    ).start();
 
-        // this.state.ov.stopAnimation(val => {
-        //     console.log('监听动画值并停止动画......',val);
-        // });
-        // this.state.ov.addListener(val => {
-        //     console.log('监听动画值并停止动画......',val);
-        // });
-    }
+    // Animated.decay(
+    //     this.state.bounceValue,{
+    //         toValue:0.2,
+    //         velocity: 0.1,      //速度
+    //         deceleration: 0.9   //哀减
+    //       }
+    // ).start();
+
+    // this.state.ov.stopAnimation(val => {
+    //     console.log('监听动画值并停止动画......',val);
+    // });
+    // this.state.ov.addListener(val => {
+    //     console.log('监听动画值并停止动画......',val);
+    // });
+  }
 
 }
 
 let styles = StyleSheet.create({
-    container:{
-        flex:1,
-        // backgroundColor:'#00ff00'
-    }
+  container: {
+    flex: 1,
+    // backgroundColor:'#00ff00'
+  }
 });
 
 export default imPage;
--- router.js
+++ router.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { Router,Tabs, Modal, Stack, Scene, ActionConst} from 'react-native-router-flux';
+import { Router,Tabs, Modal, Lightbox, Drawer, Stack, Scene, ActionConst} from 'react-native-router-flux';
+import {
+  Image,
+  Text
+} from 'react-native';
 
 import LoginPage from './compontents/loginPage/components/loginPage';
 import ForgetPage from './compontents/loginPage/components/forgetPwd';
@@ -14,45 +18,81 @@ class XSYRouter extends Component {
   render() {
     return (
       <Router>
-        <Scene key="root">
-        
-          <Modal key="modal" >
-            <Stack key="account" hideNavBar={true} init={true}>
-              <Scene key="login" init={true} component={LoginPage} title="login" hideNavBar={false} />
-              <Scene key="forget" component={ForgetPage} title="forget" hideNavBar={false} />
-              <Scene key="register" component={Register} title="register" hideNavBar={false} />
-            </Stack>
-          </Modal>
-
+        <Modal key="root">
+          
           <Tabs key="tab_main" 
-          type={ActionConst.RESET} 
-          swipeEnabled={false} 
-          animationEnabled={false}
-          >
-            <Stack key="home" hideNavBar={true} navTransparent={false} lazy={true} title="home" >
-              <Scene key="home_one" init={true} component={ImPage} title="one" hideNavBar={true} />
+            wrap={false}
+            type={ActionConst.RESET} 
+            swipeEnabled={false} 
+            animationEnabled={false} 
+            lazy={true}
+            // activeBackgroundColor="white"
+            // inactiveBackgroundColor="rgba(0, 255, 0, 0.5)"
+            activeTintColor="red"
+            inactiveTintColor="rgba(0, 255, 0, 0.5)"
+            >
+            
+            <Stack key="home" 
+            hideNavBar={true} 
+            navTransparent={false} 
+            icon={() => <Image source={require('./images/1.png')} style={{width:30,height:30}} />}
+            >
+              <Scene key="home_one" init={true} component={HomePage} title="one" hideNavBar={true} />
               <Scene key="home_two" component={PhotoPage} title="two" hideTabBar={true} hideNavBar={false} lazy={true} />
             </Stack>
-            <Stack key="message" hideNavBar={true} navTransparent={false} lazy={true}>
+            
+            <Stack key="message" 
+            hideNavBar={true} 
+            navTransparent={false} 
+            icon={() => <Image source={require('./images/2.png')} style={{width:30,height:30}} />}
+            >
               <Scene key="message_one" init={true} component={PhotoPage} title="message_one" hideNavBar={false} navTransparent={false} lazy={true} />
               <Scene key="message_two" component={ImPage} title="message_two" hideTabBar={false} hideNavBar={true} lazy={true} />
             </Stack>
-            <Stack key="crm" hideNavBar={true} navTransparent={false} lazy={true}>
+            
+            <Stack key="crm" 
+            hideNavBar={true} 
+            navTransparent={false} 
+            icon={() => <Image source={require('./images/3.jpeg')} style={{width:30,height:30}} />}
+            >
               <Scene key="crm_one" init={true} component={ImPage} title="crm_one" hideNavBar={false} navTransparent={false} lazy={true} />
               <Scene key="crm_two" component={PhotoPage} title="crm_two" hideTabBar={false} hideNavBar={true} lazy={true} />
             </Stack>
-            <Stack key="oa" hideNavBar={true} navTransparent={false} lazy={true}>
+            
+            <Stack key="oa" 
+            hideNavBar={true} 
+            navTransparent={false} 
+            icon={() => <Image source={require('./images/4.png')} style={{width:30,height:30}} />}
+            >
               <Scene key="oa_one" init={true} component={ImPage} title="oa_one" hideNavBar={false} navTransparent={false} lazy={true} />
               <Scene key="oa_two" component={PhotoPage} title="oa_two" hideTabBar={false} hideNavBar={true} lazy={true} />
             </Stack>
-            <Stack key="me" hideNavBar={true} navTransparent={false} lazy={true}>
+            
+            <Stack key="me" 
+            hideNavBar={true} 
+            navTransparent={false} 
+            icon={() => <Image source={require('./images/5.png')} style={{width:30,height:30}} />}
+            >
               <Scene key="me_one" init={true} component={PhotoPage} title="me_one" hideNavBar={false} navTransparent={false} lazy={true} />
               <Scene key="me_two" component={ImPage} title="me_two" hideTabBar={false} hideNavBar={true} lazy={true} />
             </Stack>
-
           </Tabs>
 
-        </Scene>
+          <Stack key="account" hideNavBar={true}>
+            <Scene key="login" init={true} component={LoginPage} title="login" />
+            <Scene key="forget" component={ForgetPage} title="forget" hideNavBar={false} />
+            <Scene key="register" component={Register} title="register" hideNavBar={false} />
+          </Stack>
+
+          <Drawer key="menu" drawerWidth={100}>
+            <Scene key="message_two" component={ImPage} />
+          </Drawer>
+
+          <Lightbox key="lightbox">
+            <Scene key="lightbox_one" component={LoginPage} />
+          </Lightbox>
+
+        </Modal>
       </Router>
     );
   }
