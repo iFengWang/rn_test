1a366ba0c14462b1951999645e9398d535a89460
王广峰 <wanggf@xiaoshouyi.com>
2018-01-26 17:31:36 +0800

    算法

--- index.ios.js
+++ index.ios.js
@@ -21,3 +21,28 @@ import XSYRouter from './router';
 import XSYHomePage from './compontents/homePage'
 
 AppRegistry.registerComponent('rn_test', () => XSYRouter);
+
+
+//**************************** */
+function mpSort(ary) {
+  let temp;
+  for(i=0; i<ary.length-1; i++) {
+    for(j=i+1; j<ary.length; j++) {
+      if(ary[i]>ary[j]) {
+        temp = ary[i];
+        ary[i] = ary[j];
+        ary[j] = temp;
+      }
+    }
+  }
+  return ary;
+}
+
+function quictSort(ary,i,j) {
+  let low = i, high = j;
+}
+
+let array = [3,2,6,1,7,4];
+console.log('1.....',mpSort(array));
+// quictSort(ary, 0, ary.length-1);
+//**************************** */
